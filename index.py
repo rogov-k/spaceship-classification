2dbdd0c8798b319ed54b767c9e8e571acfc1a1b9
Kirill Rogov <pluskika@gmail.com>
2020-04-07 12:52:29 +0300

    XGBClassifier + cross validation. Update: Remove label encoder

--- index.py
+++ index.py
@@ -1,12 +1,11 @@
 import csv
 
 import numpy as np
+import xgboost as xgb
 
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn import preprocessing
-import xgboost as xgb
 
 
 # #############################################################################
@@ -14,7 +13,6 @@ import xgboost as xgb
 X = []
 y = []
 sc = StandardScaler()
-le = preprocessing.LabelEncoder()
 
 with open('data/response.csv') as f:
     data = csv.reader(f)
@@ -22,10 +20,6 @@ with open('data/response.csv') as f:
         row = line[0].split(';')
         X.append(map(lambda x: float(x), row[:10]))
         y.append(row[10:][0])
-
-# Transform label to simple value (Label encoder)
-le.fit(list(set(y)))
-y = le.transform(y)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
 
@@ -45,8 +39,6 @@ y_predict = classifier.predict(X_test)
 # Set metrics
 f = open("result/XGBClassifier.result", "a")
 f.write('Cross Validation: ' + str(np.mean(cross_val_score(classifier, X_train, y_train, cv=5))) + '\n')
-f.write('Mean Absolute Error: ' + str(mean_absolute_error(y_test, y_predict)) + '\n')
-f.write('Mean Squared Error: ' + str(mean_squared_error(y_test, y_predict)) + '\n')
-f.write('Root Mean Squared Error: ' + str(np.sqrt(mean_squared_error(y_test, y_predict))) + '\n')
+f.write(classification_report(y_test, y_predict))
 f.close()
 
