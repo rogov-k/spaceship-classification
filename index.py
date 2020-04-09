2ec2dae7acb1f07743117aec27334203c3f5f0c9
Kirill Rogov <pluskika@gmail.com>
2020-04-10 01:50:27 +0300

    Decision Tree Classifier

--- index.py
+++ index.py
@@ -1,44 +1,33 @@
-import csv
-
+import pandas as pd
 import numpy as np
 
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import cross_val_score
 
 
 # #############################################################################
 # Init data
-X = []
-y = []
-sc = StandardScaler()
+train = pd.read_csv('data/train.csv', sep=';', header=0)
+y_train = train.TYPE
+X_train = train.drop('TYPE', axis=1)
 
-with open('data/response.csv') as f:
-    data = csv.reader(f)
-    for line in data:
-        row = line[0].split(';')
-        X.append(map(lambda x: float(x), row[:10]))
-        y.append(row[10:][0])
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
-
-# Feature Scaling
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+test = pd.read_csv('data/test.csv', sep=';', header=0)
+y_test = test.TYPE
+X_test = test.drop('TYPE', axis=1)
 
 # #############################################################################
 # Fit model
+
+# * Decision Tree Classifier
+print('Start: "Decision tree" classifier')
 params = {}
 classifier = DecisionTreeClassifier(**params)
 
 classifier.fit(X_train, y_train)
 y_predict = classifier.predict(X_test)
 
-# #############################################################################
-# Set metrics
 f = open("result/DecisionTreeClassifier.result", "a")
 f.write('Cross Validation: ' + str(np.mean(cross_val_score(classifier, X_train, y_train, cv=5))) + '\n\n')
 f.write(classification_report(y_test, y_predict))
 f.close()
-
