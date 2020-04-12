e91d3265b9fbc557cd14aae0bdef082e31fa7c64
Kirill Rogov <pluskika@gmail.com>
2020-04-12 04:23:33 +0300

    Базовый вариатн графиков. для каждого класса график параметров

--- index.py
+++ index.py
@@ -1,55 +1,23 @@
-import csv
-
-import numpy as np
+# coding=utf-8
 import os
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
 directory = './data/type/'
 files = os.listdir(directory)
 types = map(lambda file_name: file_name.split('.')[0], files)
 
-for file in files:
-    print(file)
-
-exit(2)
-
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import classification_report
-from sklearn.model_selection import train_test_split, cross_val_score
-
-
-# #############################################################################
-# Init data
-X = []
-y = []
-sc = StandardScaler()
-
-with open('data/response.csv') as f:
-    data = csv.reader(f)
-    for line in data:
-        row = line[0].split(';')
-        X.append(map(lambda x: float(x), row[:10]))
-        y.append(row[10:][0])
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
-
-# Feature Scaling
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-
-# #############################################################################
-# Fit model
-params = {}
-classifier = DecisionTreeClassifier(**params)
-
-classifier.fit(X_train, y_train)
-y_predict = classifier.predict(X_test)
-
-# #############################################################################
-# Set metrics
-f = open("result/DecisionTreeClassifier.result", "a")
-f.write('Cross Validation: ' + str(np.mean(cross_val_score(classifier, X_train, y_train, cv=5))) + '\n\n')
-f.write(classification_report(y_test, y_predict))
-f.close()
-
+for type in types:
+    data = pd.read_csv(directory + type + '.csv', sep=';', header=0)
+    headers = list(data.columns.values)
+    headers.pop(-1)
+    for index in headers:
+        fig = plt.figure(figsize=(30, 10))
+        plt.plot(data[index])
+        plt.title(index)
+        try:
+            os.mkdir('result/graphic/' + type + '/')
+        except OSError as error:
+            print(error)
+        plt.savefig('result/graphic/' + type + '/' + index + '.png', dpi=100)
