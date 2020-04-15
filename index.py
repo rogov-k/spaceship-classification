4702c6d0751eb3f6f6a28a9d59d5a01bca20988e
Kirill Rogov <pluskika@gmail.com>
2020-04-15 12:05:29 +0300

    Второй вариант графиков класса к параметрам

--- index.py
+++ index.py
@@ -1,23 +1,50 @@
-# coding=utf-8
-import os
 import matplotlib.pyplot as plt
 import pandas as pd
 
 
-directory = './data/type/'
-files = os.listdir(directory)
-types = map(lambda file_name: file_name.split('.')[0], files)
+classes = [
+    'Amateur',
+    'Globalstar',
+    'Human Spaceflight',
+    'Intelsat',
+    'Iridium',
+    'Navigation',
+    'Orbcomm',
+    'Weather'
+]
 
-for type in types:
-    data = pd.read_csv(directory + type + '.csv', sep=';', header=0)
-    headers = list(data.columns.values)
-    headers.pop(-1)
-    for index in headers:
-        fig = plt.figure(figsize=(30, 10))
-        plt.plot(data[index])
-        plt.title(index)
-        try:
-            os.mkdir('result/graphic/' + type + '/')
-        except OSError as error:
-            print(error)
-        plt.savefig('result/graphic/' + type + '/' + index + '.png', dpi=100)
+attributes = [
+    'MEAN_MOTION',
+    'ECCENTRICITY',
+    'INCLINATION',
+    'RA_OF_ASC_NODE',
+    'ARG_OF_PERICENTER',
+    'MEAN_ANOMALY',
+    'BSTAR',
+    'MEAN_MOTION_DOT',
+    'MEAN_MOTION_DDOT'
+]
+
+graphics_data = {}
+
+for attribute_name in attributes:
+    graphics_data[attribute_name] = {}
+    for class_name in classes:
+        graphics_data[attribute_name][class_name] = []
+
+data = pd.read_csv('data/response.csv', sep=';', header=0)
+
+for i, j in data.iterrows():
+    for attr in attributes:
+        graphics_data.get(attr).get(j['TYPE']).append(j[attr])
+
+for attribute_name, classes_ in graphics_data.items():
+    fig, image = plt.subplots(figsize=(16, 9))
+    for class_name, value in classes_.items():
+        index = classes.index(class_name)
+        image.scatter([index]*len(value), value, label=class_name, alpha=0.3, s=[10]*len(value))
+    image.legend()
+    image.set_title(attribute_name)
+    image.set_xlabel("Classes")
+    image.set_ylabel("Value")
+    fig.savefig('result/' + attribute_name + '.png', dpi=100)
