2c3a03fb0fa76079d213491f480a6c5d713aaa0e
Kirill Rogov <pluskika@gmail.com>
2020-04-30 01:15:47 +0300

    Поиск с помощью регресии идеального атрибута для разделения

--- index.py
+++ index.py
@@ -8,37 +8,77 @@ from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split, cross_val_score
 
 
-# #############################################################################
-# Init data
-X = []
-y = []
-sc = StandardScaler()
+attributes = [
+    'MEAN_MOTION',
+    'ECCENTRICITY',
+    'INCLINATION',
+    'RA_OF_ASC_NODE',
+    'ARG_OF_PERICENTER',
+    'MEAN_ANOMALY',
+    'BSTAR',
+    'MEAN_MOTION_DOT',
+    'MEAN_MOTION_DDOT'
+]
 
-with open('data/response.csv') as f:
-    data = csv.reader(f)
-    for line in data:
-        row = line[0].split(';')
-        X.append(map(lambda x: float(x), row[:10]))
-        y.append(row[10:][0])
+result = {}
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+data = pd.read_csv('./data/data.csv', sep=';', header=0)
+data = data.loc[data['TYPE'] == 'Human Spaceflight']
 
-# Feature Scaling
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+for attribute_name in attributes:
+    X = data['EPOCH']
+    y = data[attribute_name]
 
-# #############################################################################
-# Fit model
-params = {}
-classifier = DecisionTreeClassifier(**params)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+    X_train = np.array(X_train).reshape(-1, 1)
+    y_train = np.array(y_train).reshape(-1, 1)
+    X_test = np.array(X_test).reshape(-1, 1)
+    y_test = np.array(y_test).reshape(-1, 1)
 
-classifier.fit(X_train, y_train)
-y_predict = classifier.predict(X_test)
+    params = {
+        'n_estimators': 500,
+        'max_depth': 4,
+        'min_samples_split': 2,
+        'learning_rate': 0.01,
+        'loss': 'ls'
+    }
+    clf = ensemble.GradientBoostingRegressor(**params)
+    clf.fit(np.array(X_train), y_train.ravel())
+    X_test_predict = clf.predict(X_test)
 
-# #############################################################################
-# Set metrics
-f = open("result/DecisionTreeClassifier.result", "a")
-f.write('Cross Validation: ' + str(np.mean(cross_val_score(classifier, X_train, y_train, cv=5))) + '\n\n')
-f.write(classification_report(y_test, y_predict))
-f.close()
+    mse = mean_squared_error(y_test, X_test_predict)
+    r2 = r2_score(y_test, X_test_predict)
+    rmse = sqrt(mse)
 
+    result[attribute_name] = {
+        'mse': mse,
+        'r2': r2,
+        'rmse': rmse
+    }
+
+    mse = "mse: %.4f " % mse
+    r2 = "r2: %.4f " % r2
+    rmse = "rmse: %.4f " % rmse
+
+    print(attribute_name, rmse)
+
+    fig, image = plt.subplots(figsize=(16, 9))
+
+    image.set_title(mse + '; ' + rmse + '; ' + r2)
+    image.plot(y_test, label="Data")
+    image.plot(clf.predict(X_test), label="Regression")
+    image.legend()
+
+    fig.savefig('./result/' + attribute_name + '.png')
+    plt.close(fig)
+    plt.cla()
+
+min_type = ''
+min = 100
+
+for attribute, values in result.items():
+    if min > values['rmse']:
+        min_type = attribute
+        min = values['rmse']
+
+print(min_type, min)
