26ceecc1529b29a472125482de06c7db92a47ebf
Kirill Rogov <pluskika@gmail.com>
2020-04-30 05:43:55 +0300

    Добавил классификатор Naive Bayes

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ import numpy as np
 
 import xgboost as xgb
 
+from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
@@ -12,16 +13,30 @@ from sklearn.model_selection import cross_val_score
 
 # #############################################################################
 # Init data
-train = pd.read_csv('data/train.csv', sep=';', header=0)
+train = pd.read_csv('data/without_ep_type/train.csv', sep=';', header=0)
 y_train = train.TYPE
 X_train = train.drop('TYPE', axis=1)
 
-test = pd.read_csv('data/test.csv', sep=';', header=0)
+test = pd.read_csv('data/without_ep_type/test.csv', sep=';', header=0)
 y_test = test.TYPE
 X_test = test.drop('TYPE', axis=1)
 
+
 # #############################################################################
 # Fit model
+
+# * Naive Bayes Classifier
+print('Start: "Naive bayes" classifier')
+params = {}
+classifier = GaussianNB(**params)
+
+classifier.fit(X_train, y_train)
+y_predict = classifier.predict(X_test)
+
+f = open("result/NaiveBayesClassifier.result", "a")
+f.write('Cross Validation: ' + str(np.mean(cross_val_score(classifier, X_train, y_train, cv=5))) + '\n\n')
+f.write(classification_report(y_test, y_predict))
+f.close()
 
 # * Decision Tree Classifier
 print('Start: "Decision tree" classifier')
@@ -63,14 +78,14 @@ f.write(classification_report(y_test, y_predict))
 f.close()
 
 # * XGBoost Classifier
-print('Start: "XGBoost" classifier')
-params = {}
-classifier = xgb.XGBClassifier(**params)
-
-classifier.fit(X_train, y_train)
-y_predict = classifier.predict(X_test)
-
-f = open("result/XGBClassifier.result", "a")
-f.write('Cross Validation: ' + str(np.mean(cross_val_score(classifier, X_train, y_train, cv=5))) + '\n\n')
-f.write(classification_report(y_test, y_predict))
-f.close()
+# print('Start: "XGBoost" classifier')
+# params = {}
+# classifier = xgb.XGBClassifier(**params)
+#
+# classifier.fit(X_train, y_train)
+# y_predict = classifier.predict(X_test)
+#
+# f = open("result/XGBClassifier.result", "a")
+# f.write('Cross Validation: ' + str(np.mean(cross_val_score(classifier, X_train, y_train, cv=5))) + '\n\n')
+# f.write(classification_report(y_test, y_predict))
+# f.close()
